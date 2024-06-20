609d15c10958164b01b97d26721c797faa377f8c
justin <justin@garofolo.net>
2024-06-20 03:31:49 -0400

    add local file support, different types of urls (txt/html/pdf)

--- summawise/__main__.py
+++ summawise/__main__.py
@@ -1,24 +1,90 @@
-import ai, youtube, utils, validators
+import ai, youtube, validators, requests, tempfile, os
 from settings import init_settings
 from utils import DataUnit
+from pathlib import Path
 
 class NotSupportedError(Exception):
-    def __init__(self):
-        super().__init__("Failed to vectorize data. Your input type is not yet supported.")
+    def __init__(self, append: str = ""):
+        msg = "Failed to vectorize data. Your input is not yet supported" + (": " if append else ".")
+        msg += (": " if append else ".") + append
+        super().__init__(msg)
+
+def process_dir(dir_path: Path, delete: bool = True) -> str:
+    # TODO
+    _, _ = dir_path, delete
+    raise NotSupportedError()
+
+def process_file(file_path: Path, delete: bool = False) -> str:
+    # TODO(justin): 
+    # - cache vector store id based on file hash
+    # - add archive support (.zip, .tar.gz) - extract, call process_dir
+    # - maybe add automatic extraction of text from pdf or html (undecided)
+    vector_store = ai.create_vector_store(file_path.stem, [file_path])
+    _ = delete and file_path.exists() and file_path.unlink()
+    return vector_store.id
+
+def process_url(url: str) -> str:
+    if youtube.is_url(url):
+        return youtube.process(url)
+
+    extensions = {
+        "text/plain": ".txt",
+        "application/pdf": ".pdf",
+        "text/html": ".html"
+    }
+
+    response = requests.head(url, allow_redirects = True)
+    response.raise_for_status()
+
+    result_url = response.url
+    content_type = response.headers.get("Content-Type", "")
+
+    valid_types = list(extensions.keys())
+    for ctype in valid_types:
+        if content_type.startswith(ctype):
+            content_type = ctype
+            break
+
+    if not content_type in valid_types:
+        raise NotSupportedError(f"\nUnsupported content type detected from HEAD request: {content_type}")
+    
+    # send requst to download file from url (stream the data)
+    response = requests.get(result_url, stream = True)
+    response.raise_for_status()
+    
+    # create temp file and write chunks of streamed data to disk
+    extension = extensions.get(content_type, ".txt")
+    temp_file = tempfile.NamedTemporaryFile(suffix = extension, delete = False)
+    try:
+        temp_file_path = Path(temp_file.name)
+        for chunk in response.iter_content(chunk_size = 8 * DataUnit.KB):
+            temp_file.write(chunk)
+    except Exception as ex:
+        raise RuntimeError(f"Failed to write bytes to temporary file: {ex}")
+    finally:
+        temp_file.close()
+
+    # process the temp file, automatically delete it after
+    try:
+        result = process_file(temp_file_path, delete = True)
+    finally:
+        if temp_file_path.exists():
+            temp_file_path.unlink()
+    return result
 
 def process_input(input_str: str) -> str:
     """Takes user input, attempts to return OpenAI VectorStore ID after processing data."""
+    path = Path(input_str)
+    if path.exists():
+        if path.is_file():
+            return process_file(path)
+        elif path.is_dir():
+            return process_dir(path)
+
     if validators.url(input_str):
-
-        # it's a URL
-        url = input_str
-
-        # youtube url handling
-        if youtube.is_url(url):
-            return youtube.process(url)
+        return process_url(input_str)
 
     raise NotSupportedError()
-
 
 def main():
     settings = init_settings()
@@ -30,6 +96,7 @@ def main():
     while True:
         # prompt user for data source
         input_str = input("Enter a URL or local file path: ")
+        input_str = input_str.strip('\'"')
 
         # handle early exit prompts
         if input_str.lower() in ["exit", "quit", ":q"]:
@@ -42,6 +109,9 @@ def main():
             break
         except NotSupportedError as ex:
             print(ex)
+            continue
+        except (requests.RequestException, requests.HTTPError) as ex:
+            print(f"An error occurred while sending a web request to the provided URL:\n{ex}")
             continue
         except Exception as ex:
             print(f"An unhandled error occurred while processing input:\n{ex}")
