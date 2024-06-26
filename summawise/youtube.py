6115aedff839bbddde7bcba6c8d282dee51da78d
justin <justin@garofolo.net>
2024-06-26 14:01:58 -0400

    refactor Transcript/FileMetadata to inherit from Serializable base class

--- summawise/youtube.py
+++ summawise/youtube.py
@@ -1,7 +1,7 @@
 import json, re
 from . import utils, ai
-from .utils import FileUtils, fp
-from .settings import Settings, DataMode
+from .utils import DataMode, FileUtils, Serializable, fp
+from .settings import Settings
 from datetime import timedelta
 from typing import List
 from pathlib import Path
@@ -25,7 +25,7 @@ class TranscriptEntry:
             "duration": self.duration
         }
 
-class Transcript:
+class Transcript(Serializable):
 
     def __init__(
         self, 
@@ -43,35 +43,6 @@ class Transcript:
     def __str__(self):
         return "\n".join(str(entry) for entry in self.entries)
 
-    def to_json(self, pretty: bool = False) -> str:
-        return json.dumps({ 
-            "video_id": self.video_id,
-            "entries": [entry.to_dict() for entry in self.entries],
-            "vector_store_id": self.vector_store_id
-        }, indent = 4 if pretty else None)
-
-    def save_to_file(self, file_path: Path, mode: DataMode = DataMode.JSON, compress: bool = False):
-        if mode == DataMode.JSON:
-            json_str = self.to_json()
-            FileUtils.write_str(file_path, json_str, compress)
-        elif mode == DataMode.BIN:
-            FileUtils.save_object(file_path, self, compress)
-
-    @staticmethod
-    def from_file(file_path: Path, mode: DataMode = DataMode.JSON) -> "Transcript":
-        if mode == DataMode.JSON:
-            json_str = FileUtils.read_str(file_path)
-            return Transcript.from_json(json_str)
-        elif mode == DataMode.BIN:
-            return FileUtils.load_object(file_path, Transcript)
-
-    @staticmethod
-    def from_json(json_str: str) -> "Transcript":
-        data = json.loads(json_str)
-        data["entries"] = [TranscriptEntry(**entry) for entry in data.get("entries", [])]
-        transcript = Transcript(**data)
-        return transcript
-
     def vectorize(self) -> ai.VectorStore:
         name = f"transcript_{self.video_id}"
         content_path = utils.get_summawise_dir() / "youtube" / f"{name}.txt"
@@ -80,6 +51,20 @@ class Transcript:
         content_path.unlink()
         self.vector_store_id = vector_store.id
         return vector_store
+
+    def to_json(self, pretty: bool = False) -> str:
+        return json.dumps({ 
+            "video_id": self.video_id,
+            "entries": [entry.to_dict() for entry in self.entries],
+            "vector_store_id": self.vector_store_id
+        }, indent = 4 if pretty else None)
+
+    @classmethod
+    def from_json(cls, json_str: str) -> "Transcript":
+        data = json.loads(json_str)
+        data["entries"] = [TranscriptEntry(**entry) for entry in data.get("entries", [])]
+        transcript = cls(**data)
+        return transcript
 
 def get_transcript(video_id: str, vectorize: bool = False) -> Transcript:
     transcript_data = YouTubeTranscriptApi.get_transcript(video_id)
