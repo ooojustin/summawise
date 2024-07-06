72512a6c64aa6dee7f862ee026dddc73c3f6eb3b
justin <justin@garofolo.net>
2024-07-06 13:00:58 -0400

    assert_type util func to conditionally raise ValueTypeError

--- summawise/utils.py
+++ summawise/utils.py
@@ -1,5 +1,7 @@
 import tempfile
+from typing import Any, Union, Tuple
 from pathlib import Path
+from .errors import ValueTypeError
 
 class Singleton(type):
     _instances = {}
@@ -22,3 +24,20 @@ def fp(file_path: Path) -> Path:
         if gz_path.exists():
             return gz_path
     return file_path
+
+def assert_type(value: Any, type_or_types: Union[type, Tuple[type, ...]]) -> None:
+    """
+    Asserts that a variable is of a given type.
+    
+    Parameters:
+        value (Any): The variable to be checked.
+        type_or_types (Union[type, Tuple[type, ...]]): The type or types that the variable should be checked against.
+
+    Raises:
+        TypeError: If 'type_or_types' is not a type or tuple of types.
+        ValueTypeError: If the variable is not of the specified type or types.
+    """
+    if not isinstance(type_or_types, (type, tuple)):
+        raise TypeError("Function 'assert_type' parameter 'type_or_types' must be a type, or a tuple of types.")
+    elif not isinstance(value, type_or_types):
+        raise ValueTypeError(value, type_or_types)
